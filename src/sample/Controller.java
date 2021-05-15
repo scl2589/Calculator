26fdbe372900a902a2fb715ac50b3e7f9ffe714a
Chae Lin Shin <chaelinshin96@gmail.com>
2021-05-15 12:20:45 +0900

    Add calculator design

---
+++ src/sample/Controller.java
@@ -0,0 +1,16 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class Controller {
+    @FXML
+    private void operand(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void operator(ActionEvent event) {
+
+    }
+}
